5e260b3cebf46a68d1314ebfde29c8955a4c8b93
E3XP0 <hollymarijuanna@gmail.com>
2024-03-19 14:50:10 +0000

    Initial Setup

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-var express = require('express')
+const path = require("path");
+const express = require('express');
+const fs = require('fs');
+
 var app = express()
+const port = 3000;
+
+const views = path.join(__dirname, 'views');
+
 
 app.get('/', function(req, res){
-    res.sendFile("Hello World")
+    res.sendFile(views + "/index.html");
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
 });
